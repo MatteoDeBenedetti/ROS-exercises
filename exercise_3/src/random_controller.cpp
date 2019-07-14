c4ac9441055cd82cdc681666dcbfaf47c88b6c7e
Matteo De Bendetti <matteo.debendetti91@gmail.com>
2019-07-14 17:40:30 +0200

    ex3: improved pose and code refactoring

--- exercise_3/src/random_controller.cpp
+++ exercise_3/src/random_controller.cpp
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <time.h>
 
+
 //------------------------------------------------------------------------------
 // STRUCTS
 struct POSE
@@ -23,12 +24,14 @@ struct POSE
 // FUNCTIONS PROTOTYPES
 void poseCallback(const turtlesim::Pose::ConstPtr& t_pose_msg);
 float createRandomNumber(int t_seed, int min, int max);
+void avoidObstacle();
 
 
 //------------------------------------------------------------------------------
 // GLOBAL VARS
 struct POSE g_robot_pose;
 int g_seed = 20071969;
+ros::Publisher g_vel_pub;
 
 
 //------------------------------------------------------------------------------
@@ -40,7 +43,6 @@ int main(int argc, char** argv)
 
   // variables declarations
   geometry_msgs::Twist cmd_msg;
-  float Kp_theta;
 
   // variables initialization
   cmd_msg.linear.x = 0;
@@ -49,58 +51,40 @@ int main(int argc, char** argv)
 	cmd_msg.angular.x = 0;
 	cmd_msg.angular.y = 0;
 	cmd_msg.angular.z = 0;
-  Kp_theta = 1;
-
-  // get parameters
+  srand(g_seed);
+  // srand(time(NULL));
 
   // subscribe and advertise to topics
   ros::Subscriber robot_pose_sub = nh.subscribe("/turtle1/pose", 20, &poseCallback);
-  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+  g_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
   sleep(1); // wait for adv-sub
 
   // control loop
-  srand(g_seed);
-  // srand(time(NULL));
   ros::Rate loop_rate(1);
   while(ros::ok())
   {
-    // create random control
-    cmd_msg.linear.x = createRandomNumber(g_seed, -2, 2);
-    cmd_msg.angular.z = createRandomNumber(g_seed, -2, 2);
-
     // limit to upper half
-    if (g_robot_pose.y <= 6.0) /* && (
-      (g_robot_pose.theta < M_PI && g_robot_pose.linear_velocity < 0) ||
-      (g_robot_pose.theta < 2*M_PI && g_robot_pose.theta > M_PI && g_robot_pose.linear_velocity > 0) ))
-      */
+    if (g_robot_pose.y <= 6.0)
     {
-      ROS_INFO("inside if near obstacle");
-      // reorient turtle upwards
-      cmd_msg.linear.x = 0;
-      ros::Rate loop_rate2(100);
-      while (fabs(g_robot_pose.theta - M_PI/2.0) > 0.1)
-      {
-        ROS_INFO("correcting theta. err:%.2f", fabs(g_robot_pose.theta - M_PI/2.0));
-      	cmd_msg.angular.z = Kp_theta*(M_PI/2 - g_robot_pose.theta);
-        vel_pub.publish(cmd_msg);
-
-        ros::spinOnce();
-        loop_rate2.sleep();
-      }
-      ROS_INFO("theta aligned, now go up");
-      cmd_msg.angular.z = 0;
-      cmd_msg.linear.x = 1;
-      vel_pub.publish(cmd_msg);
+      ROS_INFO("WARNING: collision alert");
+      avoidObstacle();
     }
-    ROS_INFO("out of obstacle near if");
+    // move randomly
+    else
+    {
+      ROS_INFO("Moving randomly");
 
-    // send command
-    vel_pub.publish(cmd_msg);
+      // create random control
+      cmd_msg.linear.x = createRandomNumber(g_seed, -2, 2);
+      cmd_msg.angular.z = createRandomNumber(g_seed, -2, 2);
+
+      // send command
+      g_vel_pub.publish(cmd_msg);
+    }
 
     ros::spinOnce();
     loop_rate.sleep();
   }
-
 
 
   return 0;
@@ -116,15 +100,42 @@ void poseCallback(const turtlesim::Pose::ConstPtr& t_pose_msg)
   g_robot_pose.theta = fmod(2*M_PI + fmod(t_pose_msg->theta, 2*M_PI), 2*M_PI);
   g_robot_pose.linear_velocity = t_pose_msg->linear_velocity;
   g_robot_pose.angular_velocity = t_pose_msg->angular_velocity;
-
-  //ROS_INFO("theta clamped: %f", g_robot_pose.theta);
 }
 
 float createRandomNumber(int t_seed, int min, int max)
 {
   float rand_number;
-  //rand_number = rand() % ((max+abs(min)+1)*10 + min*10)/10;
   rand_number = (rand() % (max*10 + 1))/10.0*2.0 + min;
   ROS_INFO("rand number generated: %f\n", rand_number);
   return rand_number;
+}
+
+void avoidObstacle()
+{
+  geometry_msgs::Twist cmd_msg;
+  cmd_msg.linear.x = 0;
+	cmd_msg.linear.y = 0;
+	cmd_msg.linear.z = 0;
+	cmd_msg.angular.x = 0;
+	cmd_msg.angular.y = 0;
+	cmd_msg.angular.z = 0;
+  float Kp_theta = 1;
+
+  // reorient turtle upwards
+  ros::Rate loop_rate2(100);
+  while (fabs(g_robot_pose.theta - M_PI/2.0) > 0.1)
+  {
+    ROS_INFO("correcting theta: err=%.2f", fabs(g_robot_pose.theta - M_PI/2.0));
+    cmd_msg.angular.z = Kp_theta*(M_PI/2 - g_robot_pose.theta);
+    g_vel_pub.publish(cmd_msg);
+
+    ros::spinOnce();
+    loop_rate2.sleep();
+  }
+
+  // move upwards
+  ROS_INFO("theta aligned, now moving away");
+  cmd_msg.angular.z = 0;
+  cmd_msg.linear.x = 2;
+  g_vel_pub.publish(cmd_msg);
 }
